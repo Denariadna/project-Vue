641922b76657496a588a207b91e366f5dabde03f
denariadna <dar06arina@yandex.ru>
2024-12-11 00:28:27 +0300

    double-sided binding/create posts with form

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-    <div>
-        <button @click="addLike">Like</button>
-        <button @click="addDislike">Dislike</button>
-        <div>Количество лайков: {{ likes }}</div>
-        <div>Количество дизлайков: {{ dislikes }}</div>
+    <div class="app">
+        <form @submit.prevent>
+            <h2>Создать пост</h2>
+            <input v-bind:value="title" @input="title = $event.target.value" class="input" type="text"
+                placeholder="Название поста">
+            <input v-bind:value="body" @input="body = $event.target.value" class="input" type="text"
+                placeholder="Описание поста">
+            <button class="btn" @click="createPost">Создать пост</button>
+        </form>
+        <div class="post" v-for="post in posts" :key="post.id">
+            <h2>{{ post.title }}</h2>
+            <p>{{ post.description }}</p>
+        </div>
     </div>
 </template>
 
@@ -11,20 +19,67 @@
 export default {
     data() {
         return {
-            likes: 0,
-            dislikes: 0,
+            posts: [
+                { id: 1, title: 'Пост о котах', description: 'Мягкие пушистые красивые' },
+                { id: 2, title: 'Пост о котах 2', description: 'Мягкие пушистые красивые' },
+                { id: 3, title: 'Пост о котах 3', description: 'Мягкие пушистые красивые' },
+                { id: 4, title: 'Пост о котах 4', description: 'Мягкие пушистые красивые' }
+            ],
+            title: '',
+            body: '',
         }
     },
 
     methods: {
-        addLike() {
-            this.likes++;
-        },
-        addDislike() {
-            this.dislikes++;
+        createPost() {
+            const newPost = {
+                id: Date.now(),
+                title: this.title,
+                description: this.body
+            }
+            this.posts.push(newPost)
+            this.title = ''
+            this.body = ''
         }
     }
 }
 </script>
 
-<style></style>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+.app {
+    padding: 20px;
+}
+
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.btn {
+    margin-top: 15px;
+    align-self: flex-end;
+    padding: 10px 15px;
+    background: none;
+    color: turquoise;
+    border: 1px solid turquoise;
+}
+
+.input {
+    width: 100%;
+    padding: 10px;
+    border: 1px solid teal;
+    margin-top: 15px;
+}
+</style>
