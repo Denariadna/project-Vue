aa5f44a6f18eaba29c4b5cc1e0e5b261ca2e2a21
denariadna <dar06arina@yandex.ru>
2024-12-10 23:38:26 +0300

    create app and dinamic bottons

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button @click="addLike">Like</button>
+        <button @click="addDislike">Dislike</button>
+        <div>Количество лайков: {{ likes }}</div>
+        <div>Количество дизлайков: {{ dislikes }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+
+    methods: {
+        addLike() {
+            this.likes++;
+        },
+        addDislike() {
+            this.dislikes++;
+        }
+    }
+}
+</script>
+
+<style></style>
